6191cabbe61b0774160b41df4a391e07687526ea
Yaroslav Voroshilov <frqhero@gmail.com>
2023-06-19 22:46:43 +0600

    Add the bot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 
+from environs import Env
 import django
+import telegram
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
@@ -8,4 +11,9 @@ django.setup()
 from bot.models import Question
 
 if __name__ == '__main__':
-    print('Количество пропусков:', Question.objects.count())
+    env = Env()
+    env.read_env()
+    tg_bot_token = env('TG_BOT_TOKEN')
+    bot = telegram.Bot(token=tg_bot_token)
+    print(bot.get_me())
+    # print('Количество пропусков:', Question.objects.count())
