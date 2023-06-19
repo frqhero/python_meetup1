9446018970dd9b6008cc7fe040d3d94af45a0e77
Yaroslav Voroshilov <frqhero@gmail.com>
2023-06-19 23:20:50 +0600

    Add a couple commands from a tutorial

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 
 from environs import Env
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import django
-import telegram
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
@@ -10,10 +10,26 @@ django.setup()
 
 from bot.models import Question
 
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
 if __name__ == '__main__':
     env = Env()
     env.read_env()
     tg_bot_token = env('TG_BOT_TOKEN')
-    bot = telegram.Bot(token=tg_bot_token)
-    print(bot.get_me())
+    updater = Updater(token=tg_bot_token)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
     # print('Количество пропусков:', Question.objects.count())
