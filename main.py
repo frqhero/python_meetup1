62a3228734e2ec6d0acef7702928282c47077a8e
Yaroslav Voroshilov <frqhero@gmail.com>
2023-06-20 11:32:39 +0600

    Save achievment I reached learning tutorial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from environs import Env
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import django
 
@@ -12,10 +13,23 @@ from bot.models import Question
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    reply_keyboard = [['Boy', 'Girl', 'Other']]
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I'm a bot, please talk to me!",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder='Hi'
+        )
+    )
+
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 
 if __name__ == '__main__':
@@ -30,6 +44,9 @@ if __name__ == '__main__':
 
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     dispatcher.add_handler(echo_handler)
+
+    caps_handler = CommandHandler('caps', caps)
+    dispatcher.add_handler(caps_handler)
 
     updater.start_polling()
     # print('Количество пропусков:', Question.objects.count())
